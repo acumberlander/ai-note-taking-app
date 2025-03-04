08d356286327ad69eee2bf53a222121ce805e5b6
Austin <austincumberlander@gmail.com>
2025-03-04 17:00:08 -0600

    Quick update

--- client/src/components/NoteList.tsx
+++ client/src/components/NoteList.tsx
@@ -13,7 +13,7 @@ export default function NoteList() {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-      {notes.map((note) => (
+      {(notes ?? []).map((note) => (
         <Note key={note.id} note={note} />
       ))}
     </div>
