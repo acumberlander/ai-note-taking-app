e96ba3c89f4f38171337d1c5b47e5f889e49097a
Austin <austincumberlander@gmail.com>
2025-03-08 10:36:30 -0600

    Adds supabase migration

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,15 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.DATABASE_URL,
-  password: process.env.PG_PASSWORD,
-  port: Number(process.env.PG_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-// Table Schema currently
-// CREATE TABLE notes (
-//   id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-//   title VARCHAR(255),
-//   content VARCHAR(65535)
-// );
